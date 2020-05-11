d81f94f2ffa4101795953a356925f3053773138f
Iram Lee <iram.lee@gmail.com>
2020-05-11 05:09:02 -0500

    script for pi day

---
+++ coding_problems/project_e/pi_day.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from decimal import Decimal as Dec, getcontext as gc
+
+def PI(maxK = 317, prec = 4501, disp = 4500):  # Parameter defaults chosen to gain 1000+ digits within a few seconds
+    gc().prec = prec
+    K, M, L, X, S = 6, 1, 13591409, 1, 13591409
+    for k in range(1, maxK + 1):
+        M = (K**3 - 16*K) * M // k**3
+        L += 545140134
+        X *= -262537412640768000
+        S += Dec(M * L) / X
+        K += 12
+    pi = 426880 * Dec(10005).sqrt() / S
+    pi = Dec(str(pi)[:disp])  # Drop few digits of precision for accuracy
+    #print("PI(maxK={} iterations, gc().prec={}, disp={} digits) =\n{}".format(maxK, prec, disp, pi))
+    return pi
+
+
+def main():
+  pi = PI()
+  print(pi)
+
+
+if __name__ == '__main__':
+  main()
