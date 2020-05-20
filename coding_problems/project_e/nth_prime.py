33b81943fae5930cb9884fec79092fca293d9ae7
Iram Lee <iram.lee@gmail.com>
2020-05-19 19:05:35 -0500

    include optimization to get_primes

--- coding_problems/project_e/nth_prime.py
+++ coding_problems/project_e/nth_prime.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python3
 
-""" By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
-    that the 6th prime is 13.
-
-    What is the 10 001st prime number?
-"""
-
-
 def nth_prime(nth):
-  """ Find the nth prime by trial and error, using the Sieve of Eratosthenes
-      with an increasingly large size of n (where n is the limit). I.e. the
-      Sieve of Eratosthenes finds all prime numbers up to a given limit n.
-
-      Start with n = (nth * 2) and count how many primes are found. If
-      less than "nth" then increase n by a factor of 1 and start again:
-          - use a bytearray initialized with 1's
-          - set each non-prime to 0 (during the sieve of eratosthenes)
-          - after finishing running the sieve of eratosthenes:
-              just sum up the bytearray to find the total number of primes
+  """ Returns the n-th prime number
   """
-  num_primes = 0
-  trial_num = 2
-  while num_primes < nth:
-    n = (nth * trial_num)
-    primes = bytearray([1]*n)   #bytearray initialized with 1's
-    for i in range(2, n):
+  total_primes = 0
+  size_factor = 2
+  s = (nth * size_factor)
+  while total_primes < nth:
+    primes = get_primes(s)
+    total_primes = sum(primes[2:])
+    size_factor += 1
+    s = (nth * size_factor)
+  nth_prime = count_primes(primes, nth)
+  return (nth_prime, total_primes)
+
+
+def get_primes(s):
+  """ Generates primes using the Sieve of Eratosthenes
+      Includes the optimization where for every prime p, only factors p >= p^2
+      are verified.
+      The list of primes is represented with a bytearray. Each index corresponds
+      to an integer in the list. A value of "1" at the index location indicates
+      the integer is a prime.
+  """
+  primes = bytearray([1]*s)
+  for i in range(2, s):
       if primes[i] == 1:
-        for j in range(2, n):
-          if i*j < n:
+        for j in range(i, s):
+          if i*j < s:
             primes[i*j] = 0
           else:
             break
-    num_primes = sum(primes[2:])
-    trial_num += 1
+  return primes
 
+
+def count_primes(primes, nth):
+  """ Returns the n-th prime represented by the index of the n-th "1" in the
+      bytearray.
+  """
   count = 0
-  for k in range(2, n):       #find the 10,001st prime by finding the index of the
-    count += primes[k]        #10,001st "1"
+  for k in range(2, len(primes)):
+    count += primes[k]
     if count == nth:
-      return (k, num_primes)
+      return k
 
 
 def main():
-    nth = 10001
+    nth = 10000
     my_prime, total_primes = nth_prime(nth)
     print("Found {} prime numbers. The nth = {} prime is: {}".format(total_primes, nth, my_prime))
 
