7add6087a0166d9cdbc1dc389f9ae517c8195d55
Iram Lee <iram.lee@gmail.com>
2020-06-03 18:00:29 -0500

    quick implementation of Conway's game of life

---
+++ coding_problems/project_e/game_of_life.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+""" Conway's Game of Life
+"""
+import copy
+
+def game_of_life(grid_size, ticks):
+  curr_grid = [[0]*grid_size for _ in range(grid_size)]
+  next_grid = [[0]*grid_size for _ in range(grid_size)]
+  alive = False
+  field_sum = 0
+  init_setup(curr_grid)
+  for tick in range(ticks):
+    for r in range(grid_size):
+      for c in range(grid_size):
+        if r > 0:
+          field_sum += sum(curr_grid[r-1][c-1:c+2])
+        field_sum += sum(curr_grid[r][c-1:c+2])
+        if r < grid_size-1:
+          field_sum += sum(curr_grid[r+1][c-1:c+2])
+        if curr_grid[r][c] == 1:
+          alive = True
+          field_sum -= 1  #count neighbors only
+        if alive and (field_sum > 1 and field_sum <= 3):
+          next_grid[r][c] = 1
+        elif not alive and field_sum == 3:
+          next_grid[r][c] = 1
+        else:
+          next_grid[r][c] = 0
+        field_sum = 0
+        alive = False
+    curr_grid = copy.deepcopy(next_grid)
+    print_grid(curr_grid)
+
+
+def init_setup(curr_grid):
+  """Creates the initial state of the grid
+  """
+  #set up a "blinker"
+  curr_grid[4][3] = 1
+  curr_grid[4][4] = 1
+  curr_grid[4][5] = 1
+  print_grid(curr_grid)
+
+
+def print_grid(grid):
+  print("")
+  for i in range(len(grid)):
+    print(grid[i])
+  print("")
+
+
+def main():
+  game_of_life(10, 5)
+
+
+if __name__ == '__main__':
+  main()
