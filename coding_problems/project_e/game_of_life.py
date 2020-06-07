0ab66de702b0cf57ce4906d8c327a05c0f468323
Iram Lee <iram.lee@gmail.com>
2020-06-07 16:42:28 -0500

    add period to the printout

--- coding_problems/project_e/game_of_life.py
+++ coding_problems/project_e/game_of_life.py
@@ -4,13 +4,14 @@
 """
 import copy
 
-def game_of_life(grid_size, ticks):
+def game_of_life(grid_size, periods):
   curr_grid = [[0]*grid_size for _ in range(grid_size)]
   next_grid = [[0]*grid_size for _ in range(grid_size)]
   init_setup(curr_grid)
-  for tick in range(ticks):
+  for tick in range(periods):
     next_grid = next_state(curr_grid, next_grid)
     curr_grid = copy.deepcopy(next_grid)
+    print("Period: {}".format(tick+1))
     print_grid(curr_grid)
 
 
@@ -40,40 +41,23 @@ def next_state(curr_grid, next_grid):
 
 
 def init_setup(curr_grid):
-  """Creates the initial state of the grid
-  """
-  #set up a "blinker"
-  # curr_grid[4][3] = 1
-  # curr_grid[4][4] = 1
-  # curr_grid[4][5] = 1
-
-  #set up a "toad"
-  # curr_grid[3][4] = 1
-  # curr_grid[3][5] = 1
-  # curr_grid[3][6] = 1
-  # curr_grid[4][3] = 1
-  # curr_grid[4][4] = 1
-  # curr_grid[4][5] = 1
-
   #set up a "glider"
   curr_grid[1][3] = 1
   curr_grid[2][1] = 1
   curr_grid[2][3] = 1
   curr_grid[3][2] = 1
   curr_grid[3][3] = 1
-
-  print_grid(curr_grid)
+  print_grid(curr_grid)   #print the initial state
 
 
 def print_grid(grid):
-  print("")
   for i in range(len(grid)):
     print(grid[i])
-  print("")
+  print("\n")
 
 
 def main():
-  game_of_life(10, 8)
+  game_of_life(10, 20)
 
 
 if __name__ == '__main__':
